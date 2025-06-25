ce139e55d210045cf0b75b06444ee52d3ed6b8cf
Konstantin aka Grek <iamksemenov@gmail.com>
2025-06-25 14:55:55 +0300

    Launches Application and creates main window.

--- dijkstra_visual2/main.cpp
+++ dijkstra_visual2/main.cpp
@@ -4,8 +4,10 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    graphwidget w;
-    w.show();
+    QApplication a(argc,argv);
+    GraphWidget widget;
+    widget.resize(800,600);
+    widget.show();
+
     return a.exec();
 }
